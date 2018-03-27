ceaf7076da03b33512871ed4496ac117f4a9b99d
Shaun Tung <shaun.tung@gmail.com>
2018-03-27 05:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeli = [];
 function currentLine(line){
   if (line.length === 0){
-    return "The line is currently empty."
+    return console.log("The line is currently emptyempty.");return "The line is currently empty."
   }
   const numbersAndName = []
   for (let i=0, l = line.length; i < l; i++){
